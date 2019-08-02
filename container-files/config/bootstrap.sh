966e7fd02dfd2a683f8ead8e2bebc1345ce0de99
Mansalu <mitchell.steinman@utah.edu>
2019-08-02 10:54:09 -0500

    update ssh key-gen

--- container-files/config/bootstrap.sh
+++ container-files/config/bootstrap.sh
@@ -15,6 +15,7 @@ SUPERVISOR_PARAMS='-c /etc/supervisord.conf'
 ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' \
 && ssh-keygen -t dsa  -f /etc/ssh/ssh_host_dsa_key -N '' \
 && ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N '' \
+&& ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key '' \
 && chmod 600 /etc/ssh/*
 
 # Add new users
